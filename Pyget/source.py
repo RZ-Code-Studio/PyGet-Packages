efe3dbe2d031a0eb68a680eaf3264c71af87699f
RZ-Code-Studio <73451384+RZ-Code-Studio@users.noreply.github.com>
2021-07-28 20:30:33 -0700

    Update source.py

--- Pyget/source.py
+++ Pyget/source.py
@@ -8,8 +8,7 @@ import requests
 INSTALLDIR = "C:\\PyGet\\"
 
 contents = ""
-helpText = """
-Must be supplied with arguments.
+helpText = """Must be supplied with arguments.
 
 Arguments  Parameters Description
 ---------------------------------
@@ -21,18 +20,21 @@ help                   Displays this message
 
 PACKAGES_JSON_STRUCTURE = """{
     "pyget": {
-        "version": 1.0,
+        "version": 1.0
     }
-"""
+}"""
 
-def installPackagesFromManifest(manifest, installDir):
+def installPackageFromManifest(manifest, installDir):
     sourceCode = requests.get(manifest["source"]).text
     with open(installDir + manifest["name"], "wb") as file:
         file.write(sourceCode)
 
 def searchForManifest(property, value):
     pass # For now
-        
+
+def uninstallPackage(id):
+    pass # For now
+
 if os.path.isfile("packages.json"):
     with open("packages.json", "r") as file:
         contents = file.read()
@@ -40,12 +42,15 @@ elif not os.path.isfile("packages.json"):
     with open("packages.json", "w") as file:
         file.write(PACKAGES_JSON_STRUCTURE)
 
-if not sys.argv[1] or sys.argv[1] == "help":
-    print(helpText)
-else:
-    if sys.argv[1] == "install":
-        installPackageFromManifest(searchForManifest("id", sys.argv[2]), INSTALLDIR)
-    elif sys.argv[1] == "upgrade":
-        installPackageFromManifest(searchForManifest("id", sys.argv[2]), INSTALLDIR)
+try:
+    if not sys.argv[1] or sys.argv[1] == "help":
+        print(helpText)
     else:
-        uninstallPackage(sys.argv[2])
+        if sys.argv[1] == "install":
+            installPackageFromManifest(searchForManifest("id", sys.argv[2]), INSTALLDIR)
+        elif sys.argv[1] == "upgrade":
+            installPackageFromManifest(searchForManifest("id", sys.argv[2]), INSTALLDIR)
+        else:
+            uninstallPackage(sys.argv[2])
+except IndexError:
+    print(helpText)
