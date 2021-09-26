4645aaa643bb3a8609af96e90cf0d063536c5a06
RZ Code Studio <73451384+RZ-Code-Studio@users.noreply.github.com>
2021-09-25 21:33:15 -0700

    Update PyGet source code

--- PyGet/source.py
+++ PyGet/source.py
@@ -4,26 +4,27 @@ import os
 import sys
 import json
 import requests
+import shutil
 
 def main():
     __from_terminal__ = not "idlelib" in sys.modules
     
     if not __from_terminal__:
-        sys.stderr.write("MUST be ran from terminal")
-	sys.stderr.flush()
-        sys.exit(1)
+            sys.stderr.write("MUST be ran from terminal\n")
+            sys.stderr.flush()
+            sys.exit(1)
     else:
-	if sys.platform == "win32":
-		INSTALLDIR = "C:\\PyGet\\"
-	elif sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
-		INSTALLDIR = "/home/$(whoami)/PyGet/"
-	else:
-		sys.stderr.write("Unsupported OS")
-		sys.stderr.flush()
-		sys.exit(1)
+            if sys.platform == "win32":
+                INSTALLDIR = f"{os.environ['USERPRoFILE']}\\PyGet-Packages\\"
+            elif sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
+                INSTALLDIR = "/home/$(whoami)/PyGet-Packages/"
+            else:
+                sys.stderr.write("Unsupported OS\n")
+                sys.stderr.flush()
+                sys.exit(1)
 		
-	contents = ""
-	helpText = """Must be supplied with arguments.
+            contents = ""
+            helpText = """Must be supplied with arguments.
 Arguments  Parameters Description
 ---------------------------------
 upgrade   <package>    Upgrades <package>
@@ -31,41 +32,53 @@ install   <package>    Installs <package>
 uninstall <package>    Uninstalls <package>
 help                   Displays this message
 """
+            
+            def installPackageFromManifest(manifest, installPath):
+                    if not os.path.isdir(installPath + manifest["name"]):
+                        os.system(f"mkdir {installPath + manifest['name']}")
+                
+                    sourceCode = requests.get(manifest["source"]).text
+                    
+                    try:
+                        with open(installPath + manifest["name"] + "\\" + manifest["source"].split("/")[len(manifest["source"].split("/")) - 1], "wb") as file:
+                                file.write(sourceCode)
+                    except:
+                        with open(installPath + manifest["name"] + "\\" + manifest["source"].split("/")[len(manifest["source"].split("/")) - 1], "w") as file:
+                                file.write(sourceCode)
+                    
+            def searchForManifest():
+                    manifest = requests.get(f"https://raw.githubusercontent.com/RZ-Code-Studio/Pyget-Packages/main/{sys.argv[2]}/manifest.json").json()
+                    return manifest
 
-		PACKAGES_JSON_STRUCTURE = """{
-	"pyget": {
-		"version": 1.0
-	}
-}"""
+            def uninstallPackage(theName):
+                    try:
+                        shutil.rmtree(INSTALLDIR + theName)
+                    except:
+                        print("The package isn't even installed")
+                        
+            if os.path.isfile(f"{INSTALLDIR}packages.json"):
+                    with open(f"{INSTALLDIR}packages.json", "r") as file:
+                            contents = file.read()
+            elif not os.path.isfile(f"{INSTALLDIR}packages.json"):
+                    print(os.path.isdir(INSTALLDIR))
+                
+                    if not os.path.isdir(INSTALLDIR):
+                        os.system(f"mkdir {INSTALLDIR}")
+                
+                    with open(f"{INSTALLDIR}packages.json", "w") as file:
+                            file.write("{\n    \"PyGet\"{\n        \"version\": 1.0\n    }\n}")
 
-		def installPackageFromManifest(manifest, installDir):
-			sourceCode = requests.get(manifest["source"]).text
-			with open(installDir + manifest["name"], "wb") as file:
-				file.write(sourceCode)
-
-		def searchForManifest(property, value):
-			pass # For now
-
-		def uninstallPackage(id):
-			pass # For now
-
-		if os.path.isfile("packages.json"):
-			with open("packages.json", "r") as file:
-				contents = file.read()
-		elif not os.path.isfile("packages.json"):
-			with open("packages.json", "w") as file:
-				file.write(PACKAGES_JSON_STRUCTURE)
-
-		try:
-			if not sys.argv[1] or sys.argv[1] == "help":
-				print(helpText)
-			else:
-				if sys.argv[1] == "install" or sys.argv[1] == "upgrade":
-					installPackageFromManifest(searchForManifest("id", sys.argv[2]), INSTALLDIR)
-				elif sys.argv == "remove" or sys.argv == "uninstall":
-					uninstallPackage(sys.argv[2])
-		except IndexError:
-			print(helpText)
+            try:
+                 if not sys.argv[1] or sys.argv[1] == "help":
+                        print(helpText)
+                 else:
+                        if sys.argv[1] == "install" or sys.argv[1] == "upgrade":
+                                installPackageFromManifest(searchForManifest(), INSTALLDIR)
+                        elif sys.argv[1] == "remove" or sys.argv[1] == "uninstall":
+                                uninstallPackage(sys.argv[2])
+                                
+            except IndexError:
+                    print(helpText)
 
 if __name__ == "__main__":
-	main()
+    main()
