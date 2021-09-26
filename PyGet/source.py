a9d8060b8f384f14e2cb0f0848a91157504a2b12
RZ Code Studio <73451384+RZ-Code-Studio@users.noreply.github.com>
2021-09-25 21:51:32 -0700

    Update source.py

--- PyGet/source.py
+++ PyGet/source.py
@@ -6,45 +6,49 @@ import json
 import requests
 import shutil
 
+
 def main():
     __from_terminal__ = not "idlelib" in sys.modules
-    
+
     if not __from_terminal__:
         sys.stderr.write("MUST be ran from terminal\n")
         sys.stderr.flush()
         sys.exit(1)
     else:
         if sys.platform == "win32":
-            INSTALLDIR = f"{os.environ['USERPRoFILE']}\\PyGet-Packages\\"
+            INSTALLDIR = f"{os.environ['USERPROFILE']}\\PyGet-Packages\\"
         elif sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
             INSTALLDIR = "/home/$(whoami)/PyGet-Packages/"
         else:
             sys.stderr.write("Unsupported OS\n")
             sys.stderr.flush()
             sys.exit(1)
-		
+
         contents = ""
         helpText = """Must be supplied with arguments.
 Arguments  Parameters Description
 ---------------------------------
 upgrade   <package>    Upgrades <package>
-install   <package>    Installs <package>		
+install   <package>    Installs <package>
 uninstall <package>    Uninstalls <package>
 help                   Displays this message
 """
-            
-        def installPackageFromManifest(manifest, installPath):
+
+        def installPackageFromManifest(manifestURL, installPath):
             if not os.path.isdir(installPath + manifest["name"]):
                 os.system(f"mkdir {installPath + manifest['name']}")
-            
-            sourceCode = requests.get(manifest["source"]).text
-                
+
+            manifest = requests.get(manifestURL).text
+
+			for dependency in manifest["dependencies"]:
+				installPackageFromManifest(dependency, INSTALLDIR)
+
             try:
 			    with open(installPath + manifest["name"] + "\\" + manifest["source"].split("/")[len(manifest["source"].split("/")) - 1], "wb") as file:
-                    file.write(sourceCode)
+                    file.write(manifest["source"])
             except:
                 with open(installPath + manifest["name"] + "\\" + manifest["source"].split("/")[len(manifest["source"].split("/")) - 1], "w") as file:
-                    file.write(sourceCode)
+                    file.write(manifest["source"])
                     
         def searchForManifest():
             manifest = requests.get(f"https://raw.githubusercontent.com/RZ-Code-Studio/Pyget-Packages/main/{sys.argv[2]}/manifest.json").json()
